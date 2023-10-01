a3fe8d20008d57e6a14dad29c95e55fbeaaec817
Ousmane Mbaye <mbayeousmane900@gmail.com>
2023-10-01 17:33:10 +0200

    deps(cors): Added CORS to the project

--- dist/index.js
+++ dist/index.js
@@ -6,8 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const express_1 = __importDefault(require("express"));
 const Booking_1 = __importDefault(require("./services/Booking"));
+const cors_1 = __importDefault(require("cors"));
 const port = process.env.PORT || 4000;
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 app.get("/calendars/all", async (req, res) => {
     const booking = new Booking_1.default();
     const calendars = await booking.getAllCalendars();
--- lib/index.ts
+++ lib/index.ts
@@ -2,9 +2,13 @@ import "dotenv/config";
 import express from "express";
 import Booking from "./services/Booking";
 
+import cors from "cors";
+
 const port = process.env.PORT || 4000;
 
 const app = express();
+
+app.use(cors());
 
 app.get("/calendars/all", async (req, res) => {
   const booking = new Booking();
