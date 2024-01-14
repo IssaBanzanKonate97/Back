32624b9d6e1824daac62b37772427b2e173c4669
issa <issabanzan.konate@yahoo.fr>
2024-01-14 13:52:59 +0100

    ajout de mailOptionsSender de la page Contactez nous

--- lib/services/Contact/contact.service.ts
+++ lib/services/Contact/contact.service.ts
@@ -16,7 +16,19 @@ export class ContactService {
       subject: `Demande de session gratuite - ${contactForm.name}`,
       text: `Name: ${contactForm.name}\nEmail: ${contactForm.email}\nMessage: ${contactForm.message}`,
     };
+    // Email de confirmation pour l'expéditeur
+    const mailOptionsSender = {
+      from: 'support@institutadios.com',
+      to: contactForm.email,
+      subject: 'Confirmation de réception - Book free sessions with practitioners',
+      text: `Bonjour ${contactForm.name},\n\nNous avons bien reçu votre message et vous remercions de nous avoir contactés. Un membre de notre équipe va traiter votre demande et vous répondre dans les plus brefs délais.\n\nCordialement,\nL'équipe Book free sessions with practitioners`,
+  };
 
-    await this.transporter.sendMail(mailOptions);
-  }
+  // Envoyer l'email au support
+  await this.transporter.sendMail(mailOptions);
+
+  // Envoyer l'email de confirmation à l'expéditeur
+  await this.transporter.sendMail(mailOptionsSender);
+}
+
 }
